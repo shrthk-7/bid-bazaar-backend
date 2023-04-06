667b05dbeea93fa53a29fa26d97b3e21f6fac6f1
FullMetalChains <ankandutta8812@gmail.com>
2023-04-07 01:15:13 +0530

    Started Products Model,Routes and Controllers

---
+++ routes/marketplaceRouter.js
@@ -0,0 +1,9 @@
+const { userController , marketplaceController } = require('../controllers');
+const router = require('express').Router();
+
+router.get('/', marketplaceController.getProducts);
+router.get('/category/:category')
+router.get('/product/:productid', userController.authenticate, )
+
+
+module.exports = router;
